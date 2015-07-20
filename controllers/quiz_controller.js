9bac5b7a845c4e1c8b665bdf6ef06913fd957398
Sebastián Reverso <sheba1989@gmail.com>
2015-07-20 09:24:01 -0300

    Se agregó la opción para crear preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -42,3 +42,19 @@ exports.answer = function (req, res) {
 	}
 	res.render('quizes/answer', { quiz: req.quiz, respuesta: resultado });
 };
+
+//GET /quizes/new
+exports.new = function(req, res) {
+	var quiz = models.Quiz.build( // Crea el objeto Quiz
+		{ pregunta: "Pregunta", respuesta: "Respuesta" }
+	);
+	res.render('quizes/new', { quiz: quiz });
+};
+
+//POST /quizes/create
+exports.create = function(req, res) {
+	var quiz = models.Quiz.build(req.body.quiz);
+	quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
+		res.redirect('/quizes');
+	})
+};
