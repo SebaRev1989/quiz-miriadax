ea3f038229592b67168c420c0521c40d03b38bbb
Sebastián Reverso <sheba1989@gmail.com>
2015-07-20 11:29:36 -0300

    Validación de las preguntas que se agregan

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -31,7 +31,7 @@ exports.index = function(req, res) {
 
 //GET /quizes/:id
 exports.show = function (req, res) {
-	res.render('quizes/show', { quiz: req.quiz });
+	res.render('quizes/show', { quiz: req.quiz, errors: []});
 };
 
 //GET /quizes/:id/answer
@@ -40,7 +40,7 @@ exports.answer = function (req, res) {
 	if (req.query.respuesta === req.quiz.respuesta) {
 		resultado = 'Correcto';
 	}
-	res.render('quizes/answer', { quiz: req.quiz, respuesta: resultado });
+	res.render('quizes/answer', { quiz: req.quiz, respuesta: resultado, errors: []});
 };
 
 //GET /quizes/new
@@ -48,13 +48,21 @@ exports.new = function(req, res) {
 	var quiz = models.Quiz.build( // Crea el objeto Quiz
 		{ pregunta: "Pregunta", respuesta: "Respuesta" }
 	);
-	res.render('quizes/new', { quiz: quiz });
+	res.render('quizes/new', { quiz: quiz, errors: []});
 };
 
 //POST /quizes/create
 exports.create = function(req, res) {
 	var quiz = models.Quiz.build(req.body.quiz);
-	quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
-		res.redirect('/quizes');
-	})
+	quiz.validate().then(
+		function(err){
+			if (err) {
+				res.render('quizes/new', { quiz: quiz, errors: err.errors });
+			} else {
+				quiz // save: guarda en DB campos pregunta y respuesta de quiz
+				.save({fields: ["pregunta", "respuesta"]})
+				.then( function(){ res.redirect('/quizes')})
+			}
+		}
+	);
 };
