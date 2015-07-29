62048d77c139bf43034904a73fddef94398d22c0
Sebastián Reverso <sheba1989@gmail.com>
2015-07-29 08:26:40 -0300

    Se añadió índice temático a las preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -55,7 +55,7 @@ exports.answer = function (req, res) {
 //GET /quizes/new
 exports.new = function(req, res) {
 	var quiz = models.Quiz.build( // Crea el objeto Quiz
-		{ pregunta: "Pregunta", respuesta: "Respuesta" }
+		{ pregunta: "Pregunta", respuesta: "Respuesta", tema: "Tema" }
 	);
 	res.render('quizes/new', { quiz: quiz, errors: []});
 };
@@ -69,7 +69,7 @@ exports.create = function(req, res) {
 				res.render('quizes/new', { quiz: quiz, errors: err.errors });
 			} else {
 				quiz // save: guarda en DB campos pregunta y respuesta de quiz
-				.save({fields: ["pregunta", "respuesta"]})
+				.save({fields: ["pregunta", "respuesta", "tema"]})
 				.then( function(){ res.redirect('/quizes')})
 			}
 		}
@@ -86,13 +86,14 @@ exports.edit = function(req, res) {
 exports.update = function(req, res) {
 	req.quiz.pregunta = req.body.quiz.pregunta;
 	req.quiz.respuesta = req.body.quiz.respuesta;
+	req.quiz.tema = req.body.quiz.tema;
 
 	req.quiz.validate().then(
 		function (err) {
 			if (err) {
 				res.render('quizes/edit', {quiz: req.quiz, errors: err.errors });
 			} else {
-				req.quiz.save( {fields: ["pregunta", "respuesta"]} )
+				req.quiz.save( {fields: ["pregunta", "respuesta", "tema"]} )
 				.then ( function(){ res.redirect('/quizes');} );
 			}
 		}
